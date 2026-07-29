3d7760f02e16309dd765574dc08fbaf34a48ca4f
mhmdkhateeb74 <mhmdkhateeb92@gmail.com>
2026-07-29 17:53:40 +0300

    docs: add project documentation and application entry point

---
+++ index.js
@@ -0,0 +1,16 @@
+//npx expo start
+//npx expo install react-native-safe-area-context
+//npx expo install react-native-webview
+//npx expo install @react-native-async-storage/async-storage
+// npx expo install lottie-react-native
+// npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar @react-navigation/drawer react-native-gesture-handler react-native-reanimated react-native-worklets babel-preset-expo
+
+
+// import { registerRootComponent } from 'expo';
+
+// import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+// registerRootComponent(App);
